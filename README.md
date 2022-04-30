a062105f5501ce5839fc57f0bdc32a13657ca868
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2022-04-30 13:28:17 +0100

    compression algorithm implemented which compresses encryption by about 50 percent.

--- README.md
+++ README.md
@@ -29,30 +29,69 @@ require('fipamo')(key, arrKey)
 
 
 ```js
-const fipamo = require('fipamo');
-const fip = new fipamo('Secret key', ['array', 'of', 'keys.', 'Please', 'this', 'is', 'compulsory']);
+const App = require("fipamo");
 
-/* func.crypt encrypts the data. */
-console.log(fip.crypt('llslsdkdncdkdk')); // Sample string to encrypt --> result = ӳӳӬӳӬӻӴӻӱӼӻӴӻӴ
+var app = new App();
 
-/* Encoding URI enables the passing of the encrypted data string over HTTP */
-console.log(fip.encodingURI('ӳӳӬӳӬӻӴӻӱӼӻӴӻӴ')); // %D3%B3%D3%B3%D3%AC%D3%B3%D3%AC%D3%BB%D3%B4%D3%BB%D3%B1%D3%BC%D3%BB%D3%B4%D3%BB%D3%B4 
+const key = "lol", arrKeys = ["a", "b", "c", "d", "e"];
+let encryptedString = "", testString = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus rhoncus nisi, at porttitor mi pretium id. Integer vitae euismod arcu. Proin non libero in magna gravida euismod eget ac ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam gravida mi tincidunt elit tincidunt, nec blandit felis rutrum. Praesent ac iaculis ante. Nullam tincidunt nisi pretium, eleifend dui nec, tristique massa. Pellentesque molestie commodo eros, sit amet sollicitudin nunc ultrices a. Etiam luctus dignissim accumsan.`
 
-/* This function decodes the encoded URI and returns the encryted data string */
-console.log(fip.decodingURI(fip.encodingURI('ӳӳӬӳӬӻӴӻӱӼӻӴӻӴ'))); // ӳӳӬӳӬӻӴӻӱӼӻӴӻӴ
 
-/* The crypt function also decrypts the encrypted data */
-console.log(fip.crypt('ӳӳӬӳӬӻӴӻӱӼӻӴӻӴ')); // llslsdkdncdkdk
+const encryptWithoutCompression = () => {
 
-// You can also put the Secret Key and Array of keys in the function directly
-const data = {
-    key: value,
-    key1: value1,
-    key2: value2
+    encryptedString = app.crypt("hello world", key, arrKeys);
+
+    return encryptedString
+
 }
-console.log(fip.crypt(data, 'Secret key', ['array', 'of', 'keys.', 'Please', 'this', 'is', 'compulsory'])); // Sample string to encrypt --> result = ӳӳӬӳӬӻӴӻӱӼӻӴӻӴ
+
+console.log(encryptWithoutCompression(), "----- Encrypt Without Compression \n\n"); // 礫礦礯礯礬祣礴礬礱礯礧
+
+
+
+const decryptWithoutCompression = () => {
+
+    return app.crypt(encryptedString, key, arrKeys);
+
+}
+
+console.log(decryptWithoutCompression(), "----- Decrypt Without Compression \n\n"); // hello world
+
+
+const encryptWithCompression = () => {
+
+    encryptedString = app.compressCrypt(testString, key, arrKeys);
+
+    return encryptedString
+
+}
+
+const compressedPayload = encryptWithCompression()
+
+console.log(compressedPayload, "----- Encrypt With Compression \n\n"); // 礏礬礱礦礮祣礪礳礰礶硇礧礬礯硂祣礰礪礷祣礢礮礦礷祯祣礠礬礭礰礦礠礷硕礶礱硐礧硅礪礰礠礪礭礤祣礦礯硒祭祣礇硘硝祣礯礶硜礶礰祣礱礫硘礠硹祣礭硤礪硛礢硑礳硂礷礷硒硍礮礪祣礳硁砏硊硆礧硲礊礭硣礤礦硠礵硒礢礦确礶硤礮礬礧硐礱砃硲礓礱礬硩码硘硵礪礡砞礬硆礭祣礮礢礤礭礢祣礤礱礢砜礧砼礦砧礰砥砫礦砟硑礢礠硐砙礦砯硭礯礦磑礰礲礶砡礫礢礡砣砙砹硂磜祣礷礱硤砏磙砡硞礭硝礷砂硕码硢硸磲砸磕硋礢磆祣礥硗礦硸磌磦硡礳硤确砟礰礷礢礰硲礂硬磙硗磃磁磇砼砌磦硩硪礧礶磢硭硒碖礭碔碚硔磱磓礡礯礢礭硦硑礥碘硸礱礶磥硊砯磁磽磔磍磓礪磌礶硬磼磑硲礍確碦硇砏碝礪碛磢砀硓砒砐礪硊硛硭礦礪碩碤祣碛砓磮礠硛磥磫礪磩砹碊礰礢磗礯磕磛筟砥磴砏砡硙礮磉破砵礰硛硓磍硖硑礰硏硬硪磬硦砺礭碚磓確筚礠磽硐硲礆砏碌硴硺硸硦砾硤硓硇磌砃礮筜礭祭祉祉砚砘砞碆磲磕笊砲砺礢砩礶磡筡碇硂砽硁硲礎砢笂砽硸碦硂礦磲筭硌碟磾碪砞硖砙砖筍筓磔研祣碽碝砒砉砼磬礱碀硸礵硭筞磴礶磺砼碴砃碹硲礐礦磈硑磤硋磷磕笹磬硇硬砶砭硆礮礳砞硦磲砜篈礱磁祣篈礯祣碭磦砉硂碲砢磯磍碎磘磍砰礤確砼筲硐筰篲礯笃礪磽碾碌筜碀磔码硤礯硛硙硟磭硢硠箑硞硜箖箍礡礫砕筳笂磕篙筟礳碦笂磁硑篢砵篖硹篥碤笎砡笞磬硸硞礮硛礪篦篤箄硑磄硌砤礢磴砠筎碅碋硸筕码签笩礢硡碇礷篷硠筗筛础礳砂磙碇箷篭硭硛磂磀筇砼箑硦篝砖筄礪笇笅篣磌硩碴硌箣砞硤筟磔箴硛砜碋砡笓硊笐确礻磦篙磪磘确礷硲礌砩砓礵笢筈穆砄礬箠磔砄砷磳硑砸笏笉碇礳笢範箄磢砥磛硛笭笂範硿筇礪篐砂礮硹穊穈磷笠硅硋硇硡砽穊硝稏箰篔确砀硇穅硬篽碦笘碄碊祣磜砶碤碳祣礓磞硞筡磳磎篭稅稧筝筣
+
+const decryptWithCompression = () => {
+
+    encryptedString = app.decompressCrypt(encryptedString, key, arrKeys);
+
+    return encryptedString
+
+} 
+
+const decompressedPayload = decryptWithCompression();
+
+console.log(decompressedPayload, "----- Decrypt With Compression \n\n"); // Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus rhoncus nisi, at porttitor mi pretium id. Integer vitae euismod arcu. Proin non libero in magna gravida euismod eget ac ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam gravida mi tincidunt elit tincidunt, nec blandit felis rutrum. Praesent ac iaculis ante. Nullam tincidunt nisi pretium, eleifend dui nec, tristique massa. Pellentesque molestie commodo eros, sit amet sollicitudin nunc ultrices a. Etiam luctus dignissim accumsan.
 
 ```
+
+
+## Tests
+```
+npm test
+```
+
+![Fipamo test screen](https://drive.google.com/file/d/1B2u5jTh_zu48r7di3iGpp3etPKZUPhdz/view?usp=sharing)
+
 
 
 ## Open Issues / Contributions
--- index.js
+++ index.js
@@ -1,81 +1,106 @@
 class Fipamo {
-    constructor(key, arrKeys) {
-        this.key = key;
-        this.arrkeys = arrKeys;
-    }
+	constructor(key, arrKeys) {
+		this.key = key;
+		this.arrkeys = arrKeys;
+	}
 
-    // Adding a method to the constructor
-    greet(key=this.key, arrKeys=this.arrKeys){
-        return `${key} says hello.`;
-    }
-    
-    // This function is responsible for encrypting the data.
-    encode(s, k, l){
+	// Adding a method to the constructor
+	greet(key = this.key, arrKeys = this.arrKeys) {
+		return `${key} says hello.`;
+	}
+
+	// This function is responsible for encrypting the data.
+	encode(s, k, l) {
 		var enc = "";
 		var str = "";
 		// make sure that input is string
-		str = s.toString();        
-        for (var i = 0; i < s.length; i++) {
-				// create block
-				var a = s.charCodeAt(i);
-				// bitwise XOR
-				var b = a ^ (k+l);
-				enc = enc + String.fromCharCode(b);
+		str = s.toString();
+		for (var i = 0; i < s.length; i++) {
+			// create block
+			var a = s.charCodeAt(i);
+			// bitwise XOR
+			var b = a ^ (k + l);
+			enc = enc + String.fromCharCode(b);
 		}
-		
+
 		return enc;
 	}
-    
-    // This function converts individual letters to numbers
-    convertLetterToNumber(str) {
-    	var out = 0, len = str.length;
-    	for (let pos = 0; pos < len; pos++) {
-        	out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - pos - 1);
-      	}
-      	out = out.toString().replace(/\./g, '').replace(/e\+/g, '');
-      	return Number(out);
-    }
-    
-    // This function passes the string into the encode method and loops through the arrkeys property. It does both the encrypting and decrypting.
-    crypt(string, key=this.key, arrKeys=this.arrkeys) {
+
+	// This function converts individual letters to numbers
+	convertLetterToNumber(str) {
+		var out = 0, len = str.length;
+		for (let pos = 0; pos < len; pos++) {
+			out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - pos - 1);
+		}
+		out = out.toString().replace(/\./g, '').replace(/e\+/g, '');
+		return Number(out);
+	}
+
+	// This function passes the string into the encode method and loops through the arrkeys property. It does both the encrypting and decrypting.
+	crypt(string, key = this.key, arrKeys = this.arrkeys) {
 		if (typeof string === "string") {
 			string = string
 		} else {
 			string = JSON.stringify(string);
 		}
 		let enc = '';
-    	let code;
+		let code;
 		let convertedToStringOfNumbers = this.convertLetterToNumber(arrKeys.toString().replace(/,/g, ''));
-    	key = (isNaN(key)) ? this.convertLetterToNumber(key) : key;
-		if(Array.isArray(arrKeys)){
-    		let convertedToArrayOfNumbers = arrKeys.map((el) => {
-            	return this.convertLetterToNumber(el)
-        	});
-        	for(let j=0; j<convertedToArrayOfNumbers.length; j++){
-        		let ele = convertedToArrayOfNumbers[j];
-        		let ch = code || string;
-        		code = this.encode(ch, key, ele);
-        	}		
-        	enc = code;
-    	}else{
-    		enc = this.encode(string, key, convertedToStringOfNumbers)
+		key = (isNaN(key)) ? this.convertLetterToNumber(key) : key;
+		if (Array.isArray(arrKeys)) {
+			let convertedToArrayOfNumbers = arrKeys.map((el) => {
+				return this.convertLetterToNumber(el)
+			});
+			for (let j = 0; j < convertedToArrayOfNumbers.length; j++) {
+				let ele = convertedToArrayOfNumbers[j];
+				let ch = code || string;
+				code = this.encode(ch, key, ele);
+			}
+			enc = code;
+		} else {
+			enc = this.encode(string, key, convertedToStringOfNumbers)
 		}
-		try{
+		try {
 			return JSON.parse(enc);
-		} catch(err){
+		} catch (err) {
 			return enc;
 		}
 	}
-    
-    // This function encodes the generated token which is returned by encodingFunc so that it can be sent over HTTP requests.
-    encodingURI(str){
-    	return encodeURIComponent(str);
-    }
-    
-    // This function decodes a Uniform Resource Identifier (URI) component previously created by encodeURIComponent or by a similar routine.
-    decodingURI(str){
-    	return decodeURIComponent(str);
-    }
+
+	// This function encodes the generated token which is returned by encodingFunc so that it can be sent over HTTP requests.
+	encodingURI(str) {
+		return encodeURIComponent(str);
+	}
+
+	// This function decodes a Uniform Resource Identifier (URI) component previously created by encodeURIComponent or by a similar routine.
+	decodingURI(str) {
+		return decodeURIComponent(str);
+	}
+
+	// This function is used to encrypt and compress.
+	compressCrypt(string, key = this.key, arrKeys = this.arrkeys) {
+		const a = this.en(string)
+		const l = this.crypt(a, key, arrKeys);
+		return l;
+	}
+
+	//
+	decompressCrypt(string, key = this.key, arrKeys = this.arrkeys) {
+		const l = this.crypt(string, key, arrKeys);
+		const d = this.de(l);
+		return d;
+	}
+
+
+	// Compress the string
+	en(c) {
+		var x = 'charCodeAt', b, e = {}, f = c.split(""), d = [], a = f[0], g = 256;
+		for (b = 1; b < f.length; b++)c = f[b], null != e[a + c] ? a += c : (d.push(1 < a.length ? e[a] : a[x](0)), e[a + c] = g, g++, a = c); d.push(1 < a.length ? e[a] : a[x](0)); for (b = 0; b < d.length; b++)d[b] = String.fromCharCode(d[b]); return d.join("")
+	}
+
+	// Decompress the compress string
+	de(b){var a,e={},d=b.split(""),c=d[0], f=d[0],g=[c],h=256, o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
+
 }
 
 module.exports = Fipamo;
